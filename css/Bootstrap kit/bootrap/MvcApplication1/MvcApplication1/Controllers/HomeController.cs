28dcb50dd71e3ab7b4d5741720b7f8e570187058
Anil Kumar <anilmery21@gmail.com>
2023-02-11 12:51:51 +0530

    static wesite upload

---
+++ css/Bootstrap kit/bootrap/MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AboutUS()
+        {
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registration(string r)
+        {
+      
+            if (r.InsertData() == true)
+            {
+                Response.Write("<script>alert('Registration Successful')</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('Registration Failed')</script>");
+            }
+            return View();
+           
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult ABoards()
+        {
+            return View();
+        }
+    }
+}
---
+++ css/Bootstrap kit/bootrap/MvcApplication1/MvcApplication1/Models/registration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MvcApplication1.Models
+{
+    public class registration
+    {
+        public string mycommand { get; set; }
+        SqlConnection cn = new SqlConnection(@"Data Source=LENOVO;Initial Catalog=mt;Integrated Security=True");
+        public bool ExecuteInsertDeleteUpdate()
+        {
+            SqlCommand cmd = new SqlCommand(mycommand, cn);
+            if (cn.State == ConnectionState.Closed)
+            {
+                cn.Open();
+            }
+            return (cmd.ExecuteNonQuery() > 0 ? true : false);
+        }
+        public DataTable GetAllData()
+        {
+            SqlDataAdapter da = new SqlDataAdapter(mycommand, cn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+    }
+}
